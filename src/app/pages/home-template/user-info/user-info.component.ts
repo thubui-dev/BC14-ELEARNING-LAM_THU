377e3f3d4b44b119937f7c528def45ad2967ec15
lamnguyen8998 <nguyenlam2448@gmail.com>
2021-11-21 01:39:44 +0700

    lấy dữ liệu học viên , khóa học

--- src/app/pages/home-template/user-info/user-info.component.ts
+++ src/app/pages/home-template/user-info/user-info.component.ts
@@ -1,25 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { DataService } from "@services/data.service";
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-];
 
 @Component({
   selector: "app-user-info",
@@ -27,12 +8,18 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ["./user-info.component.scss"],
 })
 export class UserInfoComponent implements OnInit {
-  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
-  dataSource = ELEMENT_DATA;
   alert: boolean = false;
   constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUserCourse();
+  }
+
+  getUserCourse() {
+    this.dataService.getDetailUserCourse().subscribe((result: any) => {
+      console.log(result);
+    });
+  }
 
   registerCourse(courses: any) {
     console.log(courses);
